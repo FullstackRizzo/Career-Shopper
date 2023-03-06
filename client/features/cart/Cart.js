003f81fedc890b268c2efc5757163ab0a92e906a
wyu1277 <wyu1277@gmail.com>
2023-03-06 16:32:36 -0500

    order total updates in backend.

--- client/features/cart/Cart.js
+++ client/features/cart/Cart.js
@@ -1,14 +1,22 @@
 import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux"
-import { addToOrderQuantityAsync, deleteGuestOrderAsync, fetchGuestOrderAsync, selectCart, subtractFromOrderQuantityAsync } from "./cartSlice";
+import { addToOrderQuantityAsync, deleteGuestOrderAsync, fetchGuestOrderAsync, fetchUserOrderAsync, selectCart, subtractFromOrderQuantityAsync } from "./cartSlice";
 
 const Cart = () => {
   const dispatch = useDispatch();
+  const isLoggedIn = useSelector((state)=> !!state.auth.me.id);
+  const userId = useSelector((state)=> state.auth.me.id);
   const order = useSelector(selectCart);
-  const userId = useSelector((state) => state.auth.me.id);
+  
+  
   useEffect(()=>{
+    if(!isLoggedIn){
     dispatch(fetchGuestOrderAsync());
+    }
+    else{
+      dispatch(fetchUserOrderAsync(userId))
+    }
   },[dispatch])
 
   const handleDelete= async(id)=>{
@@ -22,30 +30,43 @@ const Cart = () => {
   const subtractFromOrderQuantity = async(id)=>{
     await dispatch(subtractFromOrderQuantityAsync(id));
   }
-
-  return (
-    <div>
-      <div id="cart-container">
-        <h1>Your Cart</h1>
-        <h2>Subtotal: ${order.reduce((acc, order) =>{
-          acc += order.career.cost * order.quantity
-          return acc;
-        },0).toLocaleString()}</h2>
-        <div id="cart-items">
-            {order.map((order) => (
-              <div key={order.career.id}>
-                <img src = {order.career.imageUrl} />
-                <h3> {order.career.name} </h3>
-                <h4> ${order.career.cost} </h4>
-                <button onClick = {e=>{e.preventDefault; subtractFromOrderQuantity(order.career.id)}}>-</button><h4> Quantity: {order.quantity} </h4><button onClick = {e=>{e.preventDefault; addToOrderQuantity(order.career.id)}}>+</button>
-                <button onClick = {e =>{e.preventDefault; handleDelete(order.career.id)}}> Delete </button>
-              </div>
-            ))}
+  
+  if(!isLoggedIn){
+    return (
+      <div>
+        <div id="cart-container">
+          <h1>Your Cart</h1>
+          <h2>Subtotal: ${order.reduce((acc, order) =>{
+            acc += order.career.cost * order.quantity
+            return acc;
+          },0).toLocaleString()}</h2>
+          <div id="cart-items">
+              {order.map((order) => (
+                <div key={order.career.id}>
+                  <img src = {order.career.imageUrl} />
+                  <h3> {order.career.name} </h3>
+                  <h4> ${order.career.cost} </h4>
+                  <button onClick = {e=>{e.preventDefault; subtractFromOrderQuantity(order.career.id)}}>-</button><h4> Quantity: {order.quantity} </h4><button onClick = {e=>{e.preventDefault; addToOrderQuantity(order.career.id)}}>+</button>
+                  <button onClick = {e =>{e.preventDefault; handleDelete(order.career.id)}}> Delete </button>
+                </div>
+              ))}
+          </div>
+          <button> Checkout </button>
         </div>
-        <button> Checkout </button>
       </div>
-    </div>
-  );
+    );
+  }
+  else{
+    return(
+      <div>
+        <div id ='cart-container'>
+          <h1>Your Cart</h1>
+          <h2>Subtotal: </h2>
+
+        </div>
+      </div>
+    )
+  }
 };
 
 export default Cart;
--- server/api/order.js
+++ server/api/order.js
@@ -21,7 +21,31 @@ router.post('/', async (req,res,next)=>{
     catch(err){
       next(err)
     }
-  })
+})
+
+router.get('/:id', async(req,res,next)=>{
+    try{
+        const order = await Order.findByPk(req.params.id);
+        res.send(order)
+    }
+    catch(err){
+        next(err)
+    }
+})
+
+router.put('/:id', async(req, res, next) => {
+    try {
+      const orderId = req.params.id;
+      const updatedOrder = req.body;
+  
+      const order = await Order.findByPk(orderId);
+  
+      const result = await order.update(updatedOrder);
+      res.json(result);
+    } catch (error) {
+      next(error);
+    }
+});
 
 
 
